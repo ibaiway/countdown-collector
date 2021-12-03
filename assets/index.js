139ecc0fc0e1a107b2f8eaf31188030361cbdaa4
Ibai Alberdi <ibaialberdi@gmail.com>
2021-12-03 02:38:23 +0100

    add new countdown works, stored in localStorage

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,11 @@
+//EventListener
+document
+  .getElementById("modalAddButton")
+  .addEventListener("click", addCountdown);
+
+//Variables
+let countdowns = getCountdowns(); //global variable of countdown objects, filled during loading of webpage from localStorage.
+
 //Toggle the visibility of the Time input in New Countdown Modal
 document.getElementById("inputTimeCheck").addEventListener("click", (e) => {
   const timeInput = document.getElementById("inputTimeDiv");
@@ -10,4 +18,35 @@ document.getElementById("inputTimeCheck").addEventListener("click", (e) => {
     timeInput.classList.toggle("visible");
   }
 });
-console.log(getCountdowns());
+
+// Recieves a title(string) and a date(Date). Returns an object with title, date(in milliseconds) and uid(current milliseconds)
+function createCountdown(title, date) {
+  return {
+    title: title,
+    date: date.getTime(),
+    uid: Date.now(),
+  };
+}
+
+// Push the countdown to global array and then save the array to localStorage
+function insertCountdown(countdown) {
+  countdowns.push(countdown);
+  setCountdowns(countdowns);
+}
+
+function addCountdown() {
+  let title = document.getElementById("inputTitle").value;
+  let date = document.getElementById("inputDate").value;
+  const checkbox = document.getElementById("inputTimeCheck").checked;
+
+  if (checkbox) {
+    let time = document.getElementById("inputTime").value;
+    date = new Date(date + "T" + time + ":00");
+  }
+
+  let countdown = createCountdown(title, date);
+  insertCountdown(countdown);
+}
+
+console.log("on load");
+console.table(getCountdowns());
