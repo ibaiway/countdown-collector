6a79e1a2fc3adff083553e3302c5cab46128b491
Ibai Alberdi <ibaialberdi@gmail.com>
2021-12-03 06:51:58 +0100

    Blinking effect added to card and colors changed depending on time

--- assets/countdown.js
+++ assets/countdown.js
@@ -1,6 +1,14 @@
 function calculateCountdown(millis) {
   const now = new Date();
-  const difference = millis > now ? millis - now : now - millis;
+  let past = false;
+  let difference = null;
+
+  if (millis > now) {
+    difference = millis - now;
+  } else {
+    difference = now - millis;
+    past = true;
+  }
 
   const days = Math.floor(difference / (1000 * 60 * 60 * 24));
   const hours = Math.floor(difference / (1000 * 60 * 60));
@@ -13,14 +21,38 @@ function calculateCountdown(millis) {
   const s = secs - mins * 60;
 
   const string = d + "d " + h + "h " + m + "m " + s + "s";
-  return string;
+  const blink = difference > 60000 ? false : true;
+  const soon = difference > 60000 ? false : true;
+
+  return {
+    coundownString: string,
+    blink: blink,
+    past: past,
+    soon: soon,
+  };
 }
 
 function updateCountdowns() {
   const countdowns = document.querySelectorAll(".countdown");
   countdowns.forEach((element) => {
-    element.innerText = calculateCountdown(
+    const countdown = calculateCountdown(
       element.getAttribute("data-countdown")
     );
+    element.innerText = countdown.coundownString;
+    countdown.blink
+      ? element.classList.add("blink")
+      : element.classList.remove("blink");
+    if (countdown.past) {
+      //set card style to danger and button to dark
+      element.parentElement.parentElement.classList.remove("bg-warning");
+      element.parentElement.parentElement.classList.add("bg-danger");
+      element.nextElementSibling.classList.remove("btn-primary");
+      element.nextElementSibling.classList.add("btn-dark");
+    } else if (countdown.soon) {
+      //set card style to warning and button to dark
+      element.parentElement.parentElement.classList.add("bg-warning");
+      element.nextElementSibling.classList.remove("btn-primary");
+      element.nextElementSibling.classList.add("btn-dark");
+    }
   });
 }
